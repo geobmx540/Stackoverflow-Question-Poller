47cc1d2d38eafcaab3010097d5c3652d06106c4e
Prescott Nasser <geobmx540@hotmail.com>
2014-11-23 15:54:37 -0800

    Basic query / poller from SO

--- Stackoverflow-Question-Poller/Program.cs
+++ Stackoverflow-Question-Poller/Program.cs
@@ -12,6 +12,12 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
 namespace Stackoverflow_Question_Poller
 {
     // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
@@ -19,6 +25,35 @@ namespace Stackoverflow_Question_Poller
     {
         static void Main()
         {
+            RunAsync().Wait();
+            Console.ReadLine();
         }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+                                         | DecompressionMethods.Deflate
+            }))
+            {
+                client.BaseAddress = new Uri("https://api.stackexchange.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var fromDate = DateTime.Now.AddDays(-5).ToUnixTime();
+                var filter = String.Format("/2.2/search?fromdate={0}&order={1}&sort={2}&site={3}&filter={4}&tagged={5}", fromDate,
+                    "desc", "activity", "stackoverflow", "withbody", "lucene.net");
+                Console.Write(filter);
+                var response = await client.GetAsync(filter);
+                if (response.IsSuccessStatusCode)
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(message);
+                }
+            }
+        }
+
     }
 }
